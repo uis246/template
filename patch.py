0541e8b991cf3ecfd95ca048adfbc72fc53a35c3
marius david <marius@mariusdavid.fr>
2023-07-24 17:54:48 +0200

    improved patch script

--- patch.py
+++ patch.py
@@ -15,10 +15,21 @@ mask = download_image("https://ponyplace.z19.web.core.windows.net/mlp/mask.png")
 mask_override = Image.open("./mask_override.png")
 autopick_override = Image.open("./pixel_override.png")
 
-autopick_white = Image.new("RGBA", (autopick.size[0], autopick.size[1]), "white");
+autopick_white = Image.new("RGBA", (autopick.size[0], autopick.size[1]), "white")
 mask.paste(autopick_white, (0, 0), autopick_override)
 mask.paste(mask_override, (0, 0), mask_override)
 
 autopick.paste(autopick_override, (0, 0), autopick_override)
+
+# make autopick transparent for black pixel in mask
+mask_override_full_black = mask_override.copy()
+mask_override_full_black_load = mask_override_full_black.load()
+for x in range(mask_override_full_black.size[0]):
+    for y in range(mask_override_full_black.size[1]):
+        if mask_override_full_black_load[x, y] != (0, 0, 0, 255):
+            mask_override_full_black_load[x, y] = (0, 0, 0, 0)
+autopick_alpha = Image.new("RGBA", (autopick.size[0], autopick.size[1]), (0, 0, 0, 0))
+autopick.paste(autopick_alpha, (0, 0), mask_override_full_black)
+
 mask.save("mask.png")
 autopick.save("autopick.png")
