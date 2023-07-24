d6644e7f1029bbf5a5b6a12c5235c846bcf714ee
uis <uis9936@gmail.com>
2023-07-24 21:06:20 +0300

    Move

--- patch.py
+++ patch.py
@@ -2,6 +2,9 @@ import requests
 import io
 from PIL import Image
 
+#Update overrides
+return -1
+
 def download_image(url):
     r = requests.get(url)
     if r.status_code != requests.codes.ok:
@@ -12,8 +15,8 @@ def download_image(url):
 autopick = download_image("https://ponyplace.z19.web.core.windows.net/mlp/autopick.png").copy()
 mask = download_image("https://ponyplace.z19.web.core.windows.net/mlp/mask.png").copy()
 
-mask_override = Image.open("./mask_override.png")
-autopick_override = Image.open("./pixel_override.png")
+mask_override = Image.open("./mask_override_ponk.png")
+autopick_override = Image.open("./pixel_override_ponk.png")
 
 autopick_white = Image.new("RGBA", (autopick.size[0], autopick.size[1]), "white")
 mask.paste(autopick_white, (0, 0), autopick_override)
